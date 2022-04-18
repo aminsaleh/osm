e83377fef7e1225eefac3e811057092f4ff02991
Amin Saleh <a.salehaghdam@gmail.com>
2022-04-18 15:12:20 +0430

    add requirement installation to bash

--- bash.sh
+++ bash.sh
@@ -1,5 +1,12 @@
+# install requirements
+pip install -r requirements.txt
+
+# set src to PYTHONPATH
 export PYTHONPATH=${PWD}
+
 mkdir data
 mkdir logs
 touch logs/log
+
+# run app
 python src/app.pys
